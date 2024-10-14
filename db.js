3bf5cbf16c6597a71d7dd9d34c2bfcd3578cf7fe
Julian <jijasarevic@gmail.com>
2024-10-14 20:21:45 -0300

    areglo base de datos + reestructuracion de carpetas

---
+++ db.js
@@ -0,0 +1,19 @@
+// database.js
+const { Sequelize } = require('sequelize');
+
+// Configurar la conexión a la base de datos
+const sequelize = new Sequelize('js.jasarevicsotomayor', 'root', '1234', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+// Probar la conexión
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión exitosa a la base de datos MySQL.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar:', err);
+    });
+
+module.exports = sequelize;
---
+++ models/materias.js
@@ -0,0 +1,24 @@
+// models/materia.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db'); // Asegúrate de que la ruta sea correcta
+
+const Materia = sequelize.define('Materia', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    cantidadAlumnos: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+}, {
+    tableName: 'materias', // Nombre de la tabla en la base de datos
+    timestamps: false, // Si no deseas campos de timestamp
+});
+
+module.exports = Materia;
